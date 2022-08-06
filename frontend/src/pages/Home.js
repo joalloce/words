ffc0be142664aaf8d8deadf92c45b70b1185264f
Jose Lo <joalloce@gmail.com>
2022-08-05 21:08:06 -0600

    tested get quotes endpoint from the frontend.

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,22 @@
+import { useEffect } from "react";
+
 const Home = () => {
-  return <div>Home</div>;
+  useEffect(() => {
+    const fetchQuotes = async () => {
+      const res = await fetch("http://localhost:4000/api/quotes");
+      const json = await res.json();
+
+      if (res.ok) {
+        console.log(json);
+      }
+    };
+    fetchQuotes();
+  }, []);
+  return (
+    <div className="home">
+      <div className="quotes">{}</div>
+    </div>
+  );
 };
 
 export default Home;
